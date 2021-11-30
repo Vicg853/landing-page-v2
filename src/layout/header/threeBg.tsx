308b5aade3f6345a2579c0ecce9e0c719abcaa85
Vicg853 <vicg853@gmail.com>
2021-11-30 20:04:02 -0300

    Created a basic version of headers three background component

--- src/layout/header/threeBg.tsx
+++ src/layout/header/threeBg.tsx
@@ -1,7 +1,10 @@
 
+import dynamic from 'next/dynamic'
 import { Canvas } from '@react-three/fiber'
 import {} from '@react-three/drei'
 import {} from '@react-spring/three'
+
+const DynamicImageImport = dynamic(() => import('./imageBg'))
 
 //TODO Add a three model import type value ++ how to include or not a property 
 export interface CustomModels<T extends ModelType> {
@@ -13,15 +16,28 @@ type ModelParameterMap = {
    'customScene': any
    'internalSrc': string
    'externalSrc': string
-   'default': false
+   'default': undefined
 }
 type ModelType = keyof ModelParameterMap
 
-export type  ThreeBackgroundProps = CustomModels<ModelType> & {
+//*Type variation in function of scene fallback type
+//* -> Ensures that the correct fallback type is passed 
+export interface CustomFallBack<T extends FallbackType> {
+   customFallback?: T
+   customFallbackType?: FallbackParameterMap[T]
+}
+type FallbackParameterMap = {
+   'component': any
+   'image': any
+}
+type FallbackType = keyof FallbackParameterMap
+
+
+export type ThreeBgProps = CustomModels<ModelType> & CustomFallBack<FallbackType> &  {
    customBackgroundColor?: string
 }
 
-const HeaderThreeScene: React.FC<ThreeBackgroundProps> = () => {
+const HeaderThreeScene: React.FC<ThreeBgProps> = () => {
    return (
       <Canvas>
          <ambientLight />
