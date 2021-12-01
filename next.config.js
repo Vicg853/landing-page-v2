a699b2791b7db46f285462955f3ef2556806d8bb
Vicg853 <vicg853@gmail.com>
2021-12-01 12:12:14 -0300

    Added support for .glb and .gltf files to be imported via webback + needed typings (still can not be imported during ssr and must be imported as dynamic in Next)

--- next.config.js
+++ next.config.js
@@ -40,6 +40,18 @@ module.exports = {
       ],
     })
 
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: "/_next/static/models",
+          outputPath: "static/models/",
+          name: '[name].[ext]'
+        }
+      },
+    })
+
     return config
   },
   async rewrites() {
---
+++ types/3Dtype.d.ts
@@ -0,0 +1,11 @@
+declare module '*.glb' {
+  const content: string;
+
+  export default content;
+}
+
+declare module '*.gltf' {
+  const content: string;
+
+  export default content;
+}
