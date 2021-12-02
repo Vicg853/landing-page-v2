a26cd4946750acafc991da33cb6df0b3f6a15d4a
Vicg853 <vicg853@gmail.com>
2021-12-02 20:44:18 -0300

    Simply updating routes

--- routes.ts
+++ routes.ts
@@ -1,112 +1,134 @@
 import type { PropsCombined } from './types/routes'
 
 const pages: PropsCombined = [
-   { 
-      name: "Home", 
-      path: "/" ,
-      navShow: true,
-      appearOnRobotsTXT: true,
-      siteMapOptions: {
-        priority: 1,
-        changeFreq: "never",
-        lastMod: "never",
-      }
-   },
-   { 
-      name: "Sobre", 
-      path: "/about" ,
-      navShow: true,
-      appearOnRobotsTXT: true,
-      siteMapOptions: {
-        priority: 1,
-        changeFreq: "never",
-        lastMod: "never",
-      }
-   },
-   { 
-      name: "Time", 
-      path: "/team" ,
-      navShow: true,
-      appearOnRobotsTXT: true,
-      siteMapOptions: {
-        priority: 2,
-        changeFreq: "never",
-        lastMod: "never",
-      },
-      navSubLinks: [
-        { title: 'Gestão', href: '/management' },
-        { title: 'Conselho', href: '/council' }
+  { 
+     name: "Home", 
+     path: "/" ,
+     navShow: true,
+     appearOnRobotsTXT: true,
+     siteMapOptions: {
+       priority: 1,
+       changeFreq: "never",
+       lastMod: "never",
+     }
+  },
+  { 
+     name: "Sobre", 
+     path: "/about" ,
+     navShow: true,
+     appearOnRobotsTXT: true,
+     siteMapOptions: {
+       priority: 1,
+       changeFreq: "never",
+       lastMod: "never",
+     }
+  },
+  { 
+     name: "Time", 
+     path: "/team" ,
+     navShow: true,
+     appearOnRobotsTXT: true,
+     siteMapOptions: {
+       priority: 2,
+       changeFreq: "never",
+       lastMod: "never",
+     },
+     navSubLinks: [
+       { title: 'Gestão', href: '/management' },
+       { title: 'Conselho', href: '/council' }
+    ]
+  },
+  { 
+     name: "ONGs", 
+     path: "/ong" ,
+     navShow: true,
+     appearOnRobotsTXT: true,
+     siteMapOptions: {
+       priority: 2,
+       changeFreq: "never",
+       lastMod: "never",
+     },
+     navSubLinks: [
+       { title: 'Arrastão', href: '/management' },
      ]
-   },
-   { 
-      name: "ONGs", 
-      path: "/ong" ,
-      navShow: true,
-      appearOnRobotsTXT: true,
-      siteMapOptions: {
-        priority: 2,
-        changeFreq: "never",
-        lastMod: "never",
-      },
-      navSubLinks: [
-        { title: 'Arrastão', href: '/management' },
-      ]
-   },
-   { 
-      name: "Doar", 
-      path: "/donate" ,
-      navShow: true,
-      appearOnRobotsTXT: true,
-      siteMapOptions: {
-        priority: 1,
-        changeFreq: "never",
-        lastMod: "never",
-      }
-   },
-   { 
-      name: "Relatorios", 
-      path: "/reports" ,
-      navShow: true,
-      appearOnRobotsTXT: true,
-      siteMapOptions: {
-        priority: 3,
-        changeFreq: "never",
-        lastMod: "never",
-      }
-   },
-   { 
-      name: "Contato", 
-      path: "/contact" ,
-      navShow: true,
-      appearOnRobotsTXT: true,
-      siteMapOptions: {
-        priority: 2,
-        changeFreq: "never",
-        lastMod: "never",
-      }
-   },
-   { 
-      name: "Blog", 
-      path: "/blog" ,
-      navShow: true,
-      appearOnRobotsTXT: true,
-      siteMapOptions: {
-        priority: 3,
-        changeFreq: "never",
-        lastMod: "never",
-      }
-   },
-   { 
-    name: "Politicas", 
-    path: "/policies" ,
-    navShow: false,
-    appearOnRobotsTXT: false,
-    siteMapOptions: {
-      priority: 1,
-      changeFreq: "never",
-      lastMod: "never",
-    }
- },
+  },
+  { 
+     name: "Doar", 
+     path: "/donate" ,
+     navShow: true,
+     appearOnRobotsTXT: true,
+     siteMapOptions: {
+       priority: 1,
+       changeFreq: "never",
+       lastMod: "never",
+     }
+  },
+  { 
+     name: "Relatorios", 
+     path: "/reports" ,
+     navShow: true,
+     appearOnRobotsTXT: true,
+     siteMapOptions: {
+       priority: 3,
+       changeFreq: "never",
+       lastMod: "never",
+     }
+  },
+  { 
+     name: "Contato", 
+     path: "/contact" ,
+     navShow: true,
+     appearOnRobotsTXT: true,
+     siteMapOptions: {
+       priority: 2,
+       changeFreq: "never",
+       lastMod: "never",
+     }
+  },
+  { 
+     name: "Blog", 
+     path: "/blog" ,
+     navShow: true,
+     appearOnRobotsTXT: true,
+     siteMapOptions: {
+       priority: 3,
+       changeFreq: "never",
+       lastMod: "never",
+     }
+  },
+  { 
+   name: "Politicas", 
+   path: "/policies" ,
+   navShow: false,
+   appearOnRobotsTXT: false,
+   siteMapOptions: {
+     priority: 1,
+     changeFreq: "never",
+     lastMod: "never",
+   }
+  },
+  { 
+   name: "Cookies", 
+   path: "/site-and-cookies" ,
+   navShow: false,
+   appearOnRobotsTXT: false,
+   siteMapOptions: {
+     priority: 1,
+     changeFreq: "never",
+     lastMod: "never",
+   }
+  },
+  { 
+   name: "techStack", 
+   path: "/site-stack" ,
+   navShow: false,
+   appearOnRobotsTXT: false,
+   siteMapOptions: {
+     priority: 2,
+     changeFreq: "never",
+     lastMod: "never",
+   }
+  },
 ];
 
 export type {PropsCombined}
