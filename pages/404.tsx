a6a3f0150dbe18cef3396a2420e0b82c5023eea1
Vicg853 <vicg853@gmail.com>
2021-12-13 03:29:09 -0300

    Added 404 and 500 error pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,63 @@
+import { Player } from "@lottiefiles/react-lottie-player"
+import styled from "styled-components"
+
+import LottieFile from '@public/lotties/404-error.json'
+
+const Container = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   min-height: 100vh;
+   width: 90vw;
+   margin-left: 5vw;
+   margin-right: 5vw;
+   .errorText {
+      font-size: 1rem;
+      margin-top: 4rem;
+      margin-bottom: 2rem;
+      font-family: 'Montserrat Alternates';
+      font-weight: 500;
+      color: var(--palette-textMain);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+   }
+   .errorCode {
+      font-family: 'Montserrat Alternates';
+      font-weight: 600;
+      font-size: 5rem;
+      color: var(--palette-textMain);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+   }      
+   .lottie {
+      width: 400px;
+      height: 400px;
+      margin-bottom: 5rem;
+   }
+`
+   
+
+
 const Err404 = () => {
-   return <h1>404 - Page Not Found</h1>
+   return (
+      <Container>
+         <span className="errorText">
+            Uhm.... você tem certeza que está no local correto?  <br/>
+            Ou... talvez, você não deveria ter chego até aqui 🧐🤫 estamos te observando... <br/>
+            Brincadeira 😉
+         </span>
+         <span className="errorCode">
+            404
+         </span>
+         <Player className="lottie" src={LottieFile} autoplay loop
+         />
+      </Container>
+   )
 }
 
 export default Err404
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,5 +1,62 @@
+import { Player } from "@lottiefiles/react-lottie-player"
+import styled from "styled-components"
+
+import LottieFile from '@public/lotties/500-error.json'
+
+const Container = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   min-height: 100vh;
+   width: 90vw;
+   margin-left: 5vw;
+   margin-right: 5vw;
+   .errorText {
+      font-size: 1rem;
+      margin-top: 4rem;
+      margin-bottom: 2rem;
+      font-family: 'Montserrat Alternates';
+      font-weight: 500;
+      color: var(--palette-textMain);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+   }
+   .errorCode {
+      font-family: 'Montserrat Alternates';
+      font-weight: 600;
+      font-size: 5rem;
+      color: var(--palette-textMain);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+   }      
+   .lottie {
+      width: 400px;
+      height: 400px;
+      margin-bottom: 5rem;
+   }
+`
+   
+
+
 const Err500 = () => {
-   return <h1>404 - Page Not Found</h1>
+   return (
+      <Container>
+         <span className="errorText">
+            Algo aconteceu... a culpa é nossa, não se preocupe<br/>
+            Devem ser aqueles guaxinins comendo os cabos do servidor novamente 😅😨 <br/>
+         </span>
+         <span className="errorCode">
+            500
+         </span>
+         <Player className="lottie" src={LottieFile} autoplay loop
+         />
+      </Container>
+   )
 }
 
 export default Err500
